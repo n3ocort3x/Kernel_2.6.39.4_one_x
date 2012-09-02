5b3b2354ae7e06eb6963f31bfceb15a335b67e94
n3ocort3x <0xn3ocort3x@gmail.com>
2012-09-02 22:28:27 +0200

    ARM: Call idle notifiers

--- arch/arm/kernel/process.c
+++ arch/arm/kernel/process.c
@@ -236,9 +236,10 @@ void cpu_idle(void)
 			}
 			bPrint_wake_lock = !bPrint_wake_lock;
 		}
+		tick_nohz_stop_sched_tick(1);
 		leds_event(led_idle_start);
 		idle_notifier_call_chain(IDLE_START);
-		tick_nohz_stop_sched_tick(1);
+		
 		while (!need_resched()) {
 #ifdef CONFIG_HOTPLUG_CPU
 			if (cpu_is_offline(smp_processor_id()))
@@ -263,8 +264,8 @@ void cpu_idle(void)
 			}
 		}
 		leds_event(led_idle_end);
-		tick_nohz_restart_sched_tick();
 		idle_notifier_call_chain(IDLE_END);
+		tick_nohz_restart_sched_tick();
 		preempt_enable_no_resched();
 		schedule();
 		preempt_disable();
